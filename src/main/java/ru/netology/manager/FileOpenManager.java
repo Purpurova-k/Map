558a231c553e2d13659dec6fdd6769fb905e5c8a
Purpurova-k <katrin.purpurova@gmail.com>
2022-02-01 13:04:16 +0300

    FileOpenManager added

---
+++ src/main/java/ru/netology/manager/FileOpenManager.java
@@ -0,0 +1,52 @@
+package ru.netology.manager;
+
+import java.util.*;
+
+public class FileOpenManager {
+
+    private Map<String, String> programsForFiles = new HashMap<>();
+
+    // Добавить новое приложение для открытия файлов с определённым расширением
+    public void addProgram(String extension, String program) {
+        if (!programsForFiles.containsKey(extension)) {
+            programsForFiles.put(extension.toLowerCase(), program);
+        } else {
+            throw new RuntimeException("Приложение " + program + " уже существует");
+        }
+    }
+
+
+    // Получить название приложения, предназначенного для открытия файла с определённым расширением
+    public String getProgram(String extension) {
+        if (programsForFiles.containsKey(extension.toLowerCase())) {
+            return programsForFiles.get(extension.toLowerCase());
+        } else {
+            throw new RuntimeException("Приложения для открытия файла с расширением " + extension.toLowerCase() + " не существует");
+        }
+    }
+
+
+    // Удалить привязку приложения к определённому расширению
+    public void deleteProgram(String extension) {
+        if (programsForFiles.containsKey(extension.toLowerCase())) {
+            programsForFiles.remove(extension.toLowerCase());
+        } else {
+            throw new RuntimeException("Приложения для файла с расширением " + extension.toLowerCase() + " не существует");
+        }
+    }
+
+
+    // Получить список всех зарегистрированных расширений, к которым привязаны приложения для открытия
+    public Set<String> getAllExtensions() {
+        List<String> sortedListExtensions = new ArrayList(programsForFiles.keySet());
+        Collections.sort(sortedListExtensions);
+        return (Set<String>) sortedListExtensions;
+    }
+
+
+    // Получить список всех приложений, которые привязаны к каким-либо расширениям
+    public Set<String> getAllPrograms() {
+        Set<String> listPrograms = new HashSet<>(programsForFiles.values());
+        return (Set<String>) listPrograms.stream().sorted();
+    }
+}
